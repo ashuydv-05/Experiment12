4e82f68dc248055ad94dbffd775dbd80b590598b
ashuydv-05 <161949281+ashuydv-05@users.noreply.github.com>
2024-09-30 00:47:43 +0530

    Create program6.cpp

---
+++ program6.cpp
@@ -0,0 +1,42 @@
+//ashu yadav
+//Prn: 23070123154
+
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class student {
+    int rno;
+    char name[50];
+    double fee;
+public:
+    student(int no, const char n[], double f);
+    student(const student &t);
+
+    void display();
+};
+
+student::student(int no, const char n[], double f) {
+    rno = no;
+    strcpy(name, n);
+    fee = f;
+}
+
+student::student(const student &t) {
+    rno = t.rno;
+    strcpy(name, t.name);
+    fee = t.fee;
+}
+
+void student::display() {
+    cout << endl << rno << "\t" << name << "\t" << fee;
+}
+
+int main() {
+    student s(1001, "Manjeet", 10000);
+    s.display();
+    student manjeet(s);
+    manjeet.display();
+    return 0;
+}
