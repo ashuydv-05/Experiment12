e1dcd655ae0d7ffef6d7d0fa4a0e709682a67a73
ashuydv-05 <161949281+ashuydv-05@users.noreply.github.com>
2024-09-30 00:43:03 +0530

    Create program1.cpp

---
+++ program1.cpp
@@ -0,0 +1,35 @@
+//ashu yadav
+//Prn: 23070123154
+
+
+#include<iostream>
+using namespace std;
+
+class student
+{
+    int rollno;
+    char name[50];
+    double fee;
+public:
+    student()
+    {
+        cout << "Enter the RollNo: ";
+        cin >> rollno;
+        cin.ignore();
+        cout << "Enter the Name: ";
+        cin.getline(name, 50);
+        cout << "Enter the Fee: "; 
+        cin >> fee;
+    }
+    void display()
+    {
+        cout << endl << rollno << "\t" << name << "\t" << fee;
+    }
+};
+
+int main()
+{
+    student s; 
+    s.display();
+    return 0;
+}
